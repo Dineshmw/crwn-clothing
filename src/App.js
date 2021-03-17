8ce81abed9deb4b5e1164043b779e02877ad3f7b
Dinesh Weerasekara <dineshweerasekara1@gmail.com>
2021-03-17 16:58:01 +0530

    integrate firebase google auth and added the sign in sign out to the header

--- src/App.js
+++ src/App.js
@@ -5,18 +5,35 @@ import HomePage from './pages/hompage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage}></Route>
-        <Route  path='/shop' component={ShopPage}></Route>
-        <Route  path='/SignIn' component={SignInAndSignUpPage}></Route>
-      </Switch>
-    </div>
-  );
+import {auth} from './firebase/firebase.utils';
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      currentUser:null
+    }
+  }
+  unsubscribeFromAuth =null
+  componentDidMount(){
+    auth.onAuthStateChanged(user =>{
+      this.setState({currentUser:user});
+    })
+  }
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+  render(){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route  path='/shop' component={ShopPage}></Route>
+          <Route  path='/SignIn' component={SignInAndSignUpPage}></Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
